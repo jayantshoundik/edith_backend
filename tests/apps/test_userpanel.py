eb5498605da73d6df1e56861522be9f2d7f02cca
Jayant Kumar <jk00910182@TechMahindra.com>
2023-03-01 21:11:55 +0530

    Test userpanel updated

--- tests/apps/test_userpanel.py
+++ tests/apps/test_userpanel.py
@@ -1,167 +1,121 @@
-from rest_framework import serializers
-from django.contrib.auth import authenticate
-from .models import *
-from django.db import models, transaction
+from rest_framework.test import APIClient
+import pytest
+from apps.userpanel.models import *
+client = APIClient()
 
 
-class RegisterSerializer(serializers.ModelSerializer):
-  role =  serializers.CharField(
-        required=True,
-        label="Role"
-    )
-  class Meta:
-    model = SwooshUser
-    fields = ('id', 'email', 'first_name', 'last_name', 'password','mobile_no','role','ps_id')
-    extra_kwargs = {'password': {'write_only': True}}
 
-  def create(self, validated_data):
-    if validated_data['email'] is not None:
-      user = SwooshUser.objects.create_user(email=validated_data['email'],mobile_no=validated_data['mobile_no'],first_name=validated_data['first_name'],last_name = validated_data['last_name'], 
-      password=validated_data['password'],ps_id=validated_data['ps_id'])
-      
-      role = Role.objects.filter(role = validated_data['role'])
-      if role.count() == 1 :
-        userrole = UserRole.objects.create(user = user,role = role.first())
-      
-      return user
-    else:
-      user = SwooshUser.objects.create_user(email=validated_data['email'],mobile_no=validated_data['mobile_no'],first_name=validated_data['first_name'],last_name = validated_data['last_name'],
-       password=validated_data['password'],ps_id=validated_data['ps_id'])
-      return user
-class SwooshUserSerializer(serializers.ModelSerializer):
-   
-    class Meta(object):
-        model = SwooshUser
-        fields = '__all__'
-
-class RoleSerializer(serializers.ModelSerializer):
-   
-    class Meta(object):
-        model = Role
-        fields = '__all__'
-
-class LoginSerializer(serializers.Serializer):
-    email = serializers.CharField(max_length=255)
-    password = serializers.CharField(max_length=128, write_only=True)
-    tokens= serializers.CharField(max_length=255, read_only=True)
-
-    class Meta(object):
-        model = SwooshUser
-        fields = ['email','password','tokens']
-    def validate(self, data):
-      
-        email = data.get('email', None)
-        password = data.get('password', None)
-
-        if email is None:
-            raise serializers.ValidationError(
-                'An email address is required to log in.'
-            )
-
-       
-        if password is None:
-            raise serializers.ValidationError(
-                'A password is required to log in.'
-            )
-
-     
-        user = authenticate(username=email, password=password)
-
-        if user is None:
-            raise serializers.ValidationError(
-                'A user with this email and password was not found.'
-            )
-
-       
-        if not user.is_active:
-            raise serializers.ValidationError(
-                'This user has been deactivated.'
-            )
-
-  
-        return user
+@pytest.mark.django_db
+def test_get_login():
+    print("sdlkfskflkflksflk")
+    payload = {
+                "username": "username",
+                "password":"Admin@123"
+            }
+    response = client.post('/api/v1/user/admin/login',payload)
+    data = response.json()
+    assert data['status'] == True
 
 
-class AppModuleCategorySerializer(serializers.ModelSerializer):
-   
-    class Meta(object):
-        model = AppModuleCategory
-        fields = '__all__'
-
-class AppModuleSerializer(serializers.ModelSerializer):
-   
-    class Meta(object):
-        model = AppModule
-        fields = '__all__'
-
-class AppModulePermissionSerializer(serializers.ModelSerializer):
-    class Meta(object):
-        model = AppModulePermission
-        fields = '__all__'
-
-class AppModuleWisePermissionListSerializer(serializers.ModelSerializer):
-    module_permissions = serializers.SerializerMethodField()
-    class Meta(object):
-        model = AppModule
-        fields = '__all__'
-    def get_module_permissions(self,obj):
-        module_permission = AppModulePermission.objects.filter(module = obj)
-        return AppModulePermissionSerializer(module_permission,many=True).data
+'''
+Test case for Create master role 
+'''
+@pytest.mark.django_db
+def test_create_role():
+    payload = {
+        "role": "OWNER",
+        "is_active": True
+    }
+    response = client.post('/api/v1/user/user/create-role', payload, format='json')
+    data = response.json()
+    assert response.status_code == 201
+    assert data['role']==payload['role']
 
 
-class CustomAppModulePermissionSerializer(serializers.Serializer):
-    id=serializers.IntegerField(required=True)
-    module_permisson_name = serializers.CharField(required=True)
+@pytest.mark.django_db
+def test_get_role_list():
+    payload = {
+        "role": "OWNER",
+        "is_active": True
+    }
+    response2 = client.post('/api/v1/user/user/create-role', payload, format='json')
+    response = client.get('/api/v1/user/user/create-role')
+    assert response.status_code == 200
 
 
-class CustomAppUserSerializer(serializers.Serializer):
-    id=serializers.IntegerField(required=True)
-    ps_id = serializers.CharField(required=True)
+@pytest.mark.django_db
+def test_create_module_category():
+    payload = {
+        "name": "TestXtract",
+        "is_active": True
+    }
+    response = client.post('/api/v1/user/admin/create-module-category', payload, format='json')
+    print(response.data)
+    data = response.json()
+    assert response.status_code == 201
+    assert data['name']==payload['name']
+
+@pytest.mark.django_db
+def test_create_module_category_list():
+    payload = {
+        "name": "TestXtract",
+        "is_active": True
+    }
+    response1 = client.post('/api/v1/user/admin/create-module-category', payload, format='json')
+    response = client.get('/api/v1/user/admin/create-module-category')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) == 1    
 
 
-class UserGroupPermissionSerializer(serializers.ModelSerializer):
-    permissions = CustomAppModulePermissionSerializer(many=True,write_only=True)
-    users = CustomAppModulePermissionSerializer(many=True,write_only=True)
- 
-    class Meta(object):
-        model = BaseGroup
-        fields = ['id','group_name','permissions','users']
-    @transaction.atomic
-    def create(self, validated_data):
-        if 'permissions' in validated_data.keys():
-            permissions = validated_data.pop('permissions')
-        # Create the base group
-        base_group_created = BaseGroup.objects.create(**validated_data)
 
-        # Deal with members data
-        if permissions is not None:
-            res_ids = [ permission['id'] for permission in permissions ]
-            perm_obj = AppModulePermission.objects.filter(id__in = res_ids)
-            basegrouppermission = BaseGroupPermissions.objects.create(
-                        base_group=base_group_created
-                    )
-            
-            basegrouppermission.app_module_permission.add(*perm_obj)
-        
-        if 'users' in validated_data.keys():
-            members = validated_data.pop('users')
-        if members is not None:
-            members_ids = [ member['id'] for member in members ]
-            perm_user_obj = SwooshUser.objects.filter(id__in = members_ids)
-         
-            basegroupmember= GroupMember.objects.get_or_create(
-                        base_group=base_group_created
-                    )
-            
-            basegroupmember.user.add(*perm_user_obj)
-            
-        return base_group_created
+@pytest.mark.django_db
+def test_create_bazaar_app_category():
+    payload = {
+        "name": "TestXtract",
+        "is_active": True
+    }
+    response = client.post('/api/v1/user/admin/create-bazaar-app-category', payload, format='json')
+    data = response.json()
+    assert response.status_code == 201
+    assert data['name']==payload['name'] 
 
-class UserProfileListSerializer(serializers.ModelSerializer):
- 
-    class Meta(object):
-        model = BaseGroupPermissions
-        fields = '__all__'
-    
 
-    
+@pytest.mark.django_db
+def test_create_bazaar_app_category_list():
+    payload = {
+        "name": "TestXtract",
+        "is_active": True
+    }
+    response1 = client.post('/api/v1/user/admin/create-bazaar-app-category', payload, format='json')
+    response = client.get('/api/v1/user/admin/create-bazaar-app-category')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) == 1 
+
+
+@pytest.mark.django_db
+def test_create_module():
+    payload = {
+        "name": "TestXtract",
+        "is_active": True
+    }
+    response = client.post('/api/v1/user/admin/create-module-category', payload, format='json')
+
+    data = response.json()
+    get_module_category = AppModuleCategory.objects.get(id=data['id'])
+    payload_module = {
+        "module": "XtractCheque",
+        "title": "Extract Cheque",
+        "description": "dsmdsnmsda ",
+        "icon": None,
+        "is_active": True,
+        "category": get_module_category.id
+    }
+    response = client.post('/api/v1/user/admin/create-module', payload_module, format='json')
+
+    data_2 = response.json()
+    print("fdsfjkdsjfkjdskf...",data_2['module'])
+    assert response.status_code == 201
+    assert data_2['module'] == payload_module['module'] 
+
